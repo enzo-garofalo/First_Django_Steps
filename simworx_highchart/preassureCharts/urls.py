7261394c715708fb860d4314282109a1ff79600f
Enzo Garofalo <161153898+enzo-garofalo@users.noreply.github.com>
2024-12-06 17:00:16 -0300

    Delete simworx_highchart directory

--- simworx_highchart/preassureCharts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PxProfundidadeView, PxTempoView
-
-urlpatterns = [
-    path('profundidade/', PxProfundidadeView.as_view(), name='PxProfundidade'),
-    path('tempo/', PxTempoView.as_view(), name='PxTempo')
-]
