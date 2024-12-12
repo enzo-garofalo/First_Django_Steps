d4aef6385e40f63f42e467f1afb4eb6e501ddf88
enzo-garofalo <dev.enzo.garofalo@gmail.com>
2024-12-12 14:24:01 -0300

    ch11 completed

--- Djangp_For_Professionals/code/ch4-bookstore/books/urls.py
+++ Djangp_For_Professionals/code/ch4-bookstore/books/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BooksListView
+from .views import BooksListView, BookDetailView
 
 urlpatterns = [
-    path('', BooksListView.as_view(), name='book_list')
+    path('', BooksListView.as_view(), name='book_list'),
+    path('<uuid:pk>', BookDetailView.as_view(), name='book_detail')
 ]
--- Djangp_For_Professionals/code/ch4-bookstore/books/views.py
+++ Djangp_For_Professionals/code/ch4-bookstore/books/views.py
@@ -1,7 +1,13 @@
 from django.views import generic
 from .models import Books
 
-class BooksListView(generic.TemplateView):
+# Para listar objetos devemos usar List View!
+class BooksListView(generic.ListView):
     model = Books
-    print(Books.objects.all())
+    context_object_name = "books_list"
     template_name = 'books/books_list.html'
+
+class BookDetailView(generic.DetailView):
+    model = Books
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'
